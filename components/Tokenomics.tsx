54262fe090f06e0d6f420933c146b9b6f8b1ded1
ramseyauron <bprasda@gmail.com>
2026-05-29 21:49:22 +0700

    Add contract address to Tokenomics component

--- components/Tokenomics.tsx
+++ components/Tokenomics.tsx
@@ -81,6 +81,9 @@ export default function Tokenomics() {
           >
             5,000,000,000 QTX total supply — fixed forever
           </p>
+          <p>
+            0xd6968214C3Bb663FB9feefb52B28d4A9Caf953fd
+          </p>
         </div>
 
         <div
